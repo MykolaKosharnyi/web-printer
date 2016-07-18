f086226bc11e10ba13fb891327ef0d0c40864b50
MykolaKosharnyi <nikolay.kosharniy@gmail.com>
2016-07-18 23:57:14 +0300

    Begin work at user pages.

---
+++ src/main/java/com/printmaster/nk/model/Role.java
@@ -0,0 +1,41 @@
+package com.printmaster.nk.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+import com.printmaster.nk.model.User;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+    private Long id;
+    private String role;
+    private Set<User> users;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @ManyToMany(mappedBy = "roles")
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
+    }
+}
--- src/main/java/com/printmaster/nk/model/User.java
+++ src/main/java/com/printmaster/nk/model/User.java
@@ -2,13 +2,9 @@ package com.printmaster.nk.model;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.Set;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="user")
@@ -44,6 +40,13 @@ public class User implements Serializable{
 	
 	@Column(name="time_registration")
 	private Date timeRegistration;
+	
+	@Transient
+	private String passwordConfirm;
+	
+	@ManyToMany
+    @JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles;
 	
 	public User(){
 	}
