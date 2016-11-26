f2fb95c4708f91332083baf0c2af370bb949925d
MykolaKosharnyi <nikolay.kosharniy@gmail.com>
2016-11-26 22:55:04 +0200

    File title.js was changed by using principle DRY.

--- src/main/webapp/resources/js/user/title.js
+++ src/main/webapp/resources/js/user/title.js
@@ -101,66 +101,74 @@ $( function() {
 /* Вивід в ліве меню назв продукту і їх ссилок */
 jQuery(document).ready(function($) {
 	
-		/* For printers */	
-		$( "li#list_dissolving ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_dissolving').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_ecosolvent ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_ecosolvent').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_UV_roll ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_UV_roll').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_UV_flatbed ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_UV_flatbed').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_sublimation ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_sublimation').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_textile ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_textile').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_water_pigment ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_water_pigment').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_digital_printers ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_digital_printers').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_SAPR-GIS_printers ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_SAPR-GIS_printers').addClass( "menu_list_list" ) : "" ;
-		
-		/* For 3d printers */
-		$( "li#list_3d_printers_FDM-extruder ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_3d_printers_FDM-extruder').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_3d_printers_photo_printing_polyjet ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_3d_printers_photo_printing_polyjet').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_3d_printers_laser_sintering_LENS ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_3d_printers_laser_sintering_LENS').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_3d_printers_lamination_LOM ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_3d_printers_lamination_LOM').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_3d_printers_stereolithography_SL ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_3d_printers_stereolithography_SL').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_3d_printers_laser_sintering_LS ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_3d_printers_laser_sintering_LS').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_3d_printers_powder_bonding_3DP ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_3d_printers_powder_bonding_3DP').addClass( "menu_list_list" ) : "" ;
-		
-		/* For digital printers */
-		$( "li#list_digital_printer_FullColorLaserPrinters ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_digital_printer_FullColorLaserPrinters').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_digital_printer_MonochromeLaserPrinters ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_digital_printer_MonochromeLaserPrinters').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_digital_printer_FullColorInkjetPrinters ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_digital_printer_FullColorInkjetPrinters').addClass( "menu_list_list" ) : "" ;
+	function menuHasProductInSection(array){
+
+		array.forEach(function(sub_section) {
+			$( "li#" + sub_section + " ul.menu_drop_drop" ).has( "li" ).length ? $( "li#" + sub_section).addClass( "menu_list_list" ) : "" ;
+		});
+				
+	}
 	
-		/* For lasers */
-		$( "li#list_laser_CO2_gas_lasers ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_laser_CO2_gas_lasers').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_laser_solid_state_lasers ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_laser_solid_state_lasers').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_laser_for_the_treatment_of_metal ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_laser_for_the_treatment_of_metal').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_laser_diode_pumped ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_laser_diode_pumped').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_laser_fiber_lasers ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_laser_fiber_lasers').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_laser_plasma_lasers ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_laser_plasma_lasers').addClass( "menu_list_list" ) : "" ;
+	/* For printers */	
+	menuHasProductInSection(["list_dissolving", 
+	                         "list_ecosolvent",
+	                         "list_UV_roll",
+	                         "list_UV_flatbed",
+	                         "list_sublimation",
+	                         "list_textile",
+	                         "list_water_pigment",
+	                         "list_digital_printers",
+	                         "list_SAPR-GIS_printers"]);
+		
+	/* For 3d printers */
+	menuHasProductInSection(["list_3d_printers_FDM-extruder", 
+	                         "list_3d_printers_photo_printing_polyjet",
+	                         "list_3d_printers_laser_sintering_LENS",
+	                         "list_3d_printers_lamination_LOM",
+	                         "list_3d_printers_stereolithography_SL",
+	                         "list_3d_printers_laser_sintering_LS",
+	                         "list_3d_printers_powder_bonding_3DP"]);
+		
+	/* For digital printers */
+	menuHasProductInSection(["list_digital_printer_FullColorLaserPrinters", 
+	                         "list_digital_printer_MonochromeLaserPrinters",
+	                         "list_digital_printer_FullColorInkjetPrinters"]);
 	
-		/* For cutters */
-		$( "li#list_cutter_for_wood ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_cutter_for_wood').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_cutter_for_the_treatment_of_metal ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_cutter_for_the_treatment_of_metal').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_cutter_stone_processing ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_cutter_stone_processing').addClass( "menu_list_list" ) : "" ;
+	/* For lasers */
+	menuHasProductInSection(["list_laser_CO2_gas_lasers", 
+	                         "list_laser_solid_state_lasers",
+	                         "list_laser_for_the_treatment_of_metal",
+	                         "list_laser_diode_pumped",
+	                         "list_laser_fiber_lasers",
+	                         "list_laser_plasma_lasers"]);
+	
+	/* For cutters */
+	menuHasProductInSection(["list_cutter_for_wood", 
+	                         "list_cutter_for_the_treatment_of_metal",
+	                         "list_cutter_stone_processing"]);
+	
+	/* For laminators */
+	menuHasProductInSection(["list_laminators_hot_lamination", 
+	                         "list_laminators_cold_laminating",
+	                         "list_laminators_liquid",
+	                         "list_laminators_flatbed_laminating_machine"]);
 		
-		/* For laminators */
-		$( "li#list_laminators_hot_lamination ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_laminators_hot_lamination').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_laminators_cold_laminating ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_laminators_cold_laminating').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_laminators_liquid ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_laminators_liquid').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_laminators_flatbed_laminating_machine ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_laminators_flatbed_laminating_machine').addClass( "menu_list_list" ) : "" ;
+	/* For scanners */
+	menuHasProductInSection(["list_scanner_large_format_scanners", 
+	                         "list_scanner_3d_scanners"]);
 		
-		/* For scanners */
-		$( "li#list_scanner_large_format_scanners ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_scanner_large_format_scanners').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_scanner_3d_scanners ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_scanner_3d_scanners').addClass( "menu_list_list" ) : "" ;
+	/* For rips */
+	menuHasProductInSection(["list_rip_printing_equipment", 
+	                         "list_rip_3D_printers",
+	                         "list_rip_laser_milling_equipment",
+	                         "list_rip_3D_scanners",
+	                         "list_rip_scanners"]);
 		
-		/* For rips */
-		$( "li#list_rip_printing_equipment ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_rip_printing_equipment').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_rip_3D_printers ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_rip_3D_printers').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_rip_laser_milling_equipment ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_rip_laser_milling_equipment').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_rip_3D_scanners ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_rip_3D_scanners').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_rip_scanners ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_rip_scanners').addClass( "menu_list_list" ) : "" ;
-		
-		/* For used with product */
-		$( "li#list_use_with_product_ink_for_inkjet ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_use_with_product_ink_for_inkjet').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_use_with_product_consumables_for_digital_equipment ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_use_with_product_consumables_for_digital_equipment').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_use_with_product_consumables_for_3D_equipment ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_use_with_product_consumables_for_3D_equipment').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_use_with_product_products_for_maintenance ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_use_with_product_products_for_maintenance').addClass( "menu_list_list" ) : "" ;
-		$( "li#list_use_with_product_parts_and_accessories ul.menu_drop_drop" ).has( "li" ).length ? $( 'li#list_use_with_product_parts_and_accessories').addClass( "menu_list_list" ) : "" ;
+	/* For used with product */
+	menuHasProductInSection(["list_use_with_product_ink_for_inkjet", 
+	                         "list_use_with_product_consumables_for_digital_equipment",
+	                         "list_use_with_product_consumables_for_3D_equipment",
+	                         "list_use_with_product_products_for_maintenance",
+	                         "list_use_with_product_parts_and_accessories"]);
 		
 }); // ready()
