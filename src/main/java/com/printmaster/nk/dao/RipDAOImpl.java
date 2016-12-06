6f509c7b462ac331145afefd3392c2f3b67b92f8
MykolaKosharnyi <nikolay.kosharniy@gmail.com>
2016-12-07 01:40:31 +0200

    Make in RipDAOImpl more clearly to understand how works searching on user page.

--- src/main/java/com/printmaster/nk/dao/RipDAOImpl.java
+++ src/main/java/com/printmaster/nk/dao/RipDAOImpl.java
@@ -113,136 +113,157 @@ public class RipDAOImpl implements ProductDAO<Rip, SearchRips> {
 
 		cr.add(Restrictions.eq("showOnSite", true));
 
-		HashSet<Rip> result = new HashSet<Rip>(cr.list());
+		return deleteUncheckedRip(new HashSet<Rip>(cr.list()), usedDate, searchRips);
+	}
 
+	private HashSet<Rip> deleteUncheckedRip(HashSet<Rip> result, JSONArray usedDate, SearchRips searchRips){
+		//get all program product with which can showed to consumer
 		Iterator<Rip> itRip = result.iterator();
 
 		while (itRip.hasNext()) {
-			Rip currentRip = itRip.next();
-			boolean isRipWeNeed = false;
-
-			if (searchRips.getTypeEquipment() != null) {
-				if (searchRips.getTypeEquipment().length > 0) {
-
-					rip: for (String typeEquipment : searchRips.getTypeEquipment()) {
-						if (currentRip.getTypeEquipment().equals(typeEquipment)) {
-
-							if (searchRips.getSoftwareMaker() != null) {
-								if (searchRips.getSoftwareMaker().length > 0) {
-
-									Iterator<JSONObject> typeEquipmetnJson = usedDate.iterator();
-
-									while (typeEquipmetnJson.hasNext()) {
-										JSONObject concreteEquipment = typeEquipmetnJson.next();
-										if (concreteEquipment.get("type_equipment").equals(typeEquipment)) {
-
-											JSONArray arrayEquipment = (JSONArray) concreteEquipment.get("software_maker");
-
-											// Checking if type equipment no has checked position, in this case we
-											// output all product in this checked type equip.
-											Iterator<JSONObject> itSoftwareCheck = arrayEquipment.iterator();
-
-											JSONArray checkedSoftware = new JSONArray();
-											while (itSoftwareCheck.hasNext()) {
-												checkedSoftware.add(itSoftwareCheck.next().get("name"));
-											}
-
-											int countSuitableSoftware = 0;
-											for (String softwareMaker : searchRips.getSoftwareMaker()) {
-												if (checkedSoftware.contains(softwareMaker))
-													countSuitableSoftware++;
-											}
-
-											if (countSuitableSoftware <= 0) {
-												isRipWeNeed = true;
-												break rip;
-											}
-
-											Iterator<JSONObject> itSoftware = arrayEquipment.iterator();
-											while (itSoftware.hasNext()) {
-												JSONObject concreteSoftware = itSoftware.next();
-
-												for (String softwareMaker : searchRips.getSoftwareMaker()) {
-
-													if (concreteSoftware.get("name").equals(softwareMaker)
-														&& currentRip.getSoftwareMaker().equals(softwareMaker)) {
-
-															if (searchRips.getSoftwareClass() != null) {
-																if (searchRips.getSoftwareClass().length > 0) {
-																	JSONArray arraySoftClass = (JSONArray) concreteSoftware.get("software_class");
-
-																	// Check if software class has checked in place we need
-																	int countSuitableSoftwareClass = 0;
-																	for (String softwareClass : searchRips.getSoftwareClass()) {
-																		if (arraySoftClass.contains(softwareClass))
-																			countSuitableSoftwareClass++;
-																	}
-
-																	if (countSuitableSoftwareClass <= 0) {
-																		isRipWeNeed = true;
-																		break rip;
-																	}
-
-																			for (String softwareClass : searchRips.getSoftwareClass()) {
-																				if (arraySoftClass.contains(softwareClass)
-																						&& currentRip.getSoftwareClass().equals(softwareClass)) {
-	
-																					isRipWeNeed = true;
-																					break rip;
-																			}
-																			}
-																		
-																	
-																} else {
-																	isRipWeNeed = true;
-																	break rip;
-																}
-															} else {
-																isRipWeNeed = true;
-																break rip;
-															}
-															break rip;
-														}
-												}
-
-											}
-											break rip;
-										}
-									}
-								} else {
-									isRipWeNeed = true;
-									break rip;
-								}
-							} else {
-								isRipWeNeed = true;
-								break rip;
-							}
-							break rip;
-						}
-					}
-				} else {
-					isRipWeNeed = true;
-				}
-
-			} else {
-				isRipWeNeed = true;
-			}
-
-			if ((!isRipWeNeed))
+			if ((!checkByTypeEquipmentLevel(itRip.next(), usedDate, searchRips)))
 				itRip.remove();
 		}
-
-		// if(searchRips.getSoftwareVersion()!= null){
-		// Junction softwareVersionGroup = Restrictions.disjunction();
-		// for(String softwareVersion : searchRips.getSoftwareVersion()){
-		// softwareVersionGroup.add(Restrictions.eq("softwareVersion",softwareVersion));
-		// }
-		// cr.add(softwareVersionGroup);
-		// }
-
 		return result;
 	}
+	
+	/**
+	 * Check program product to search criteria (begin from type equipment level)
+	 * 
+	 * @param currentRip given to check
+	 * @param usedDate date about program product in rip.json
+	 * @param searchRips criteria to search program product
+	 * @return true if currentRip required searchRips criteria
+	 */
+	private boolean checkByTypeEquipmentLevel(Rip currentRip, JSONArray usedDate, SearchRips searchRips){
+		boolean isRipWeNeed = false;
 
+		// check if user checked any of TYPE EQUIPMENT if don't so out all
+		// products
+		if ((searchRips.getTypeEquipment() != null) && (searchRips.getTypeEquipment().length > 0)) {
+
+			for (String typeEquipment : searchRips.getTypeEquipment()) 
+				if (currentRip.getTypeEquipment().equals(typeEquipment))
+					return checkBySoftwareMakerLevel(currentRip, usedDate, searchRips, isRipWeNeed, typeEquipment);
+
+		} else {
+			return true;
+		}
+		return isRipWeNeed;
+	}
+
+	/**
+	 * Check program product to search criteria (work on software maker level)
+	 * 
+	 * @param currentRip given to check
+	 * @param usedDate  date about program product in rip.json
+	 * @param searchRips criteria to search program product
+	 * @param isRipWeNeed default value (if given product don't respond given criteria so delete this product)
+	 * @param typeEquipment concrete checked equipment from searchRips
+	 * @return true if currentRip required searchRips criteria
+	 */
+	@SuppressWarnings("unchecked")
+	private boolean checkBySoftwareMakerLevel(Rip currentRip, JSONArray usedDate, SearchRips searchRips,
+			boolean isRipWeNeed, String typeEquipment) {
+		if ( (searchRips.getSoftwareMaker() != null) && (searchRips.getSoftwareMaker().length > 0) ) {
+
+			Iterator<JSONObject> typeEquipmetnJson = usedDate.iterator();
+
+			while (typeEquipmetnJson.hasNext()) {
+				JSONObject concreteEquipment = typeEquipmetnJson.next();
+				if (concreteEquipment.get("type_equipment").equals(typeEquipment)) {
+
+					JSONArray arrayEquipment = (JSONArray) concreteEquipment.get("software_maker");
+
+					if(isNotCheckedSoftwareForThisEquipment(searchRips, arrayEquipment)) return true;
+
+					Iterator<JSONObject> itSoftware = arrayEquipment.iterator();
+					while (itSoftware.hasNext()) {
+						JSONObject concreteSoftware = itSoftware.next();
+
+						for (String softwareMaker : searchRips.getSoftwareMaker()) 
+							if (concreteSoftware.get("name").equals(softwareMaker)
+									&& currentRip.getSoftwareMaker().equals(softwareMaker)) 
+								return checkBySoftwareClassLevel(currentRip, searchRips, isRipWeNeed, concreteSoftware);
+					}
+					return isRipWeNeed;
+				}
+			}
+
+		} else {
+			return true;
+		}
+		return isRipWeNeed;
+	}
+
+	/**
+	 * Check program product to search criteria (work on software class level)
+	 * 
+	 * @param currentRip given to check
+	 * @param searchRips criteria to search program product
+	 * @param isRipWeNeed default value (if given product don't respond given criteria so delete this product)
+	 * @param concreteSoftware - software maker which contain these software classes
+	 * @return true if currentRip required searchRips criteria
+	 */
+	private boolean checkBySoftwareClassLevel(Rip currentRip, SearchRips searchRips, boolean isRipWeNeed,
+			JSONObject concreteSoftware) {
+		if ( (searchRips.getSoftwareClass() != null) && (searchRips.getSoftwareClass().length > 0) ) {
+				JSONArray arraySoftClass = (JSONArray) concreteSoftware.get("software_class");
+
+				if(isNotCheckeSoftwareClassesForThisSoftwareMaker(searchRips, arraySoftClass)) return true;
+
+				for (String softwareClass : searchRips.getSoftwareClass()) {
+					if (arraySoftClass.contains(softwareClass) && currentRip.getSoftwareClass().equals(softwareClass)) 
+						return true;														
+				}
+
+		} else {
+			return true;
+		}
+		return isRipWeNeed;
+	}
+
+	/**
+	 * Checking if software classes was checked for this concrete software maker
+	 * 
+	 * @param searchRips
+	 * @param arraySoftClass
+	 * @return if nothing checked than add this product
+	 */
+	private boolean isNotCheckeSoftwareClassesForThisSoftwareMaker(SearchRips searchRips, JSONArray arraySoftClass) {
+		for (String softwareClass : searchRips.getSoftwareClass()) 
+			if (arraySoftClass.contains(softwareClass))
+				return false;
+		
+		return true;
+	}
+
+	/**
+	 * 	Checking if type equipment no has checked position,
+	 *  in this case we output all product in this checked type equip.
+	 * @param searchRips
+	 * @param arrayEquipment
+	 * @return if nothing checked than add this product
+	 */
+	@SuppressWarnings("unchecked")
+	private boolean isNotCheckedSoftwareForThisEquipment(SearchRips searchRips, JSONArray arrayEquipment) {
+
+		Iterator<JSONObject> itSoftwareCheck = arrayEquipment.iterator();
+
+		JSONArray checkedSoftware = new JSONArray();
+		while (itSoftwareCheck.hasNext()) {
+			checkedSoftware.add(itSoftwareCheck.next().get("name"));
+		}
+
+		int countSuitableSoftware = 0;
+		for (String softwareMaker : searchRips.getSoftwareMaker()) {
+			if (checkedSoftware.contains(softwareMaker))
+				countSuitableSoftware++;
+		}
+
+		return (countSuitableSoftware <= 0) ? true:false;
+	}
+	
 	@SuppressWarnings("unchecked")
 	@Override
 	public Set<Rip> listShowOnSite() {
