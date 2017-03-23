04aa4e14ecf9ffc889fc68d54f76327e152d7828
MykolaKosharnyi <nikolay.kosharniy@gmail.com>
2017-03-24 01:34:10 +0200

    Set to MailSenderController methods for manipulating mail notification receivers.

--- src/main/java/com/printmaster/nk/components/MailSendingComponent.java
+++ src/main/java/com/printmaster/nk/components/MailSendingComponent.java
@@ -4,6 +4,8 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.Iterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.mail.Address;
 import javax.mail.Message;
@@ -27,6 +29,10 @@ public class MailSendingComponent {
 	
 	@Autowired
     private JavaMailSenderImpl mailSender;
+	
+	private static final String EMAIL_PATTERN =
+			"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
 	
 	private final static String HOST_EMAIL = "noreplay@forprint.net.ua";
 	private final static String ADMIN_EMAIL = "nikolay.kosharniy@gmail.com";
@@ -79,6 +85,27 @@ public class MailSendingComponent {
 	     final PrintWriter pw = new PrintWriter(sw, true);
 	     throwable.printStackTrace(pw);
 	     return sw.getBuffer().toString();
+	}
+	
+	/**
+	 * Validate email with regular expression
+	 *
+	 * @param email for validation
+	 * @return true valid email, false invalid email
+	 */
+	private boolean isEmailCorrect(String email){
+		String result = email.trim();
+		Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+		Matcher matcher = pattern.matcher(result);
+		return matcher.matches();
+	}
+	
+	public boolean checkEmail(JSONArray array, String email){		
+		return !isParameterRepeated(array, email) && isEmailCorrect(email);
+	}
+	
+	private boolean isParameterRepeated(JSONArray array, String email){
+		return array.contains(email);
 	}
 	
 }
--- src/main/java/com/printmaster/nk/controller/CommentsController.java
+++ src/main/java/com/printmaster/nk/controller/CommentsController.java
@@ -1,12 +1,7 @@
 package com.printmaster.nk.controller;
 
-import java.util.Collections;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import javax.validation.Valid;
 
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -17,7 +12,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.printmaster.nk.beans.ComponentsForControllers;
@@ -37,10 +31,6 @@ public class CommentsController {
 	@Autowired
     ComponentsForControllers componets;
 	
-	private static final String EMAIL_PATTERN =
-			"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-	
 	private final static String USERS_JSON_FILE_NAME = "user_mail_receiver";
 
 	@RequestMapping(value="/admin/comments", method = RequestMethod.GET)
@@ -54,62 +44,6 @@ public class CommentsController {
 		commentService.delete(id);	
         return "redirect:/admin/comments";
     }  
-	
-	@RequestMapping(value="/admin/change_comment_recipient_notification", method = RequestMethod.GET)
-    public String changeCommentRecipientNotification(Model model){
-		model.addAttribute("emails", componets.jsonArrayParser(USERS_JSON_FILE_NAME));
-        return "admin/change_comment_recipient_notification";
-    } 
-	
-	@SuppressWarnings("unchecked")
-	@RequestMapping(value = "/admin/comments/add_recipient_notification", method = RequestMethod.POST)
-	public String addRecipientNotificationEmail(@RequestParam(value = "new_email") String email) {
-		JSONArray array = componets.jsonArrayParser(USERS_JSON_FILE_NAME);
-		array.add(email);	
-		Collections.sort(array);
-		componets.saveObject(array, USERS_JSON_FILE_NAME);
-	    return "redirect:/admin/change_comment_recipient_notification";
-	}
-	
-	@RequestMapping(value = "/admin/comments/remove_recipient_notification", method = RequestMethod.POST,
-    		consumes = MediaType.TEXT_PLAIN_VALUE)
-	public @ResponseBody void removeRecipientNotificationEmail(@RequestBody String email) {
-		JSONArray array = componets.jsonArrayParser(USERS_JSON_FILE_NAME);
-		array.remove(email);	
-		componets.saveObject(array, USERS_JSON_FILE_NAME);
-	}
-	
-	@SuppressWarnings("unchecked")
-	@RequestMapping(value="/admin/comments/check_email", method = RequestMethod.POST,
-    		produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.TEXT_PLAIN_VALUE)
-    public @ResponseBody JSONObject checkEmailC(@RequestBody String email) {
-    	JSONObject result = new JSONObject();
-    	JSONArray array = componets.jsonArrayParser(USERS_JSON_FILE_NAME);
-    	result.put("result", checkEmail(array, email));
-    	return result;
-    }
-	
-	private boolean checkEmail(JSONArray array, String email){		
-		return !isParameterRepeated(array, email) && isEmailCorrect(email);
-	}
-	
-	private boolean isParameterRepeated(JSONArray array, String email){
-		return array.contains(email);
-	}
-	
-	/**
-	 * Validate email with regular expression
-	 *
-	 * @param email
-	 *            email for validation
-	 * @return true valid email, false invalid email
-	 */
-	private boolean isEmailCorrect(String email){
-		String result = email.trim();
-		Pattern pattern = Pattern.compile(EMAIL_PATTERN);
-		Matcher matcher = pattern.matcher(result);
-		return matcher.matches();
-	}
 	
 	@RequestMapping(value="/comment/add", method = RequestMethod.POST)
     public @ResponseBody Comment addNewComment(@ModelAttribute("addComment") @Valid Comment comment) {	
@@ -133,7 +67,6 @@ public class CommentsController {
     		mailSendingComponent.observeRecipients("Удален комментарий", getCommentBody( commentWichWillBeDeleted ),
     				componets.jsonArrayParser(USERS_JSON_FILE_NAME));
     	}
-    	
     	return result;
     }
 	
