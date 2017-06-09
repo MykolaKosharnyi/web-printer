ec939ed2bdf7461c2cc74b4b7ac6e6b2de3711e9
MykolaKosharnyi <nikolay.kosharniy@gmail.com>
2017-06-09 07:42:08 +0300

    Create user hierarchy, create abstract class AbsctractUser.

--- src/main/java/com/printmaster/nk/model/entity/User.java
+++ src/main/java/com/printmaster/nk/model/entity/User.java
@@ -1,59 +1,14 @@
 package com.printmaster.nk.model.entity;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 import javax.persistence.*;
-
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 
 @Entity
 @Table(name="user")
-public class User implements Serializable{
-	
-	private static final long serialVersionUID = -357378939455722167L;
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name="id")
-	private Long id;
-	
-	@Column(name="firstname")
-	private String firstName;
-	
-	@Column(name="lastname")
-	private String lastname;
-	
-	@Column(name="county")
-	private String county;
-	
-	@Column(name="city")
-	private String city;
-	
+public class User extends AbstractUser{
+	private static final long serialVersionUID = 2779875230120306603L;
+
 	@Column(name="password")
 	private String password;
-	
-	@Column(nullable = false, columnDefinition = "bit default 1")
-	private boolean enabled;
-	
-	@Column(name="telephone")
-	private String telephone;
-	
-	@Column(name="company")
-	private String company;
-	
-	@Column(name="email")
-	private String email;
-	
-	@Column(name="date_of_birth_bay")
-	private Date dateOfBirthDay;
-	
-	@Column(name="time_registration")
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date timeRegistration = new Date();
 	
 	@Transient
 	private String passwordConfirm;
@@ -64,24 +19,7 @@ public class User implements Serializable{
 	@Column(name="nameUserPicture")
 	private String nameUserPicture;
 	
-	@ElementCollection(fetch = FetchType.EAGER)
-	@Column(name="subscription")
-	@Fetch(value = FetchMode.SUBSELECT)
-	private List<String> subscription = new ArrayList<String>();
-	
-	@ElementCollection(fetch = FetchType.EAGER)
-	@Column(name="scopeOfActivities")
-	private List<String> scopeOfActivities = new ArrayList<String>();
-	
 	public User(){}
-
-	public boolean isEnabled() {
-		return enabled;
-	}
-
-	public void setEnabled(boolean enabled) {
-		this.enabled = enabled;
-	}
 
 	public String getNameUserPicture() {
 		return nameUserPicture;
@@ -89,14 +27,6 @@ public class User implements Serializable{
 
 	public void setNameUserPicture(String nameUserPicture) {
 		this.nameUserPicture = nameUserPicture;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
 	}
 
 	public String getPasswordConfirm() {
@@ -123,103 +53,8 @@ public class User implements Serializable{
 		this.password = password;
 	}
 
-	public Date getDateOfBirthDay() {
-		return dateOfBirthDay;
-	}
-
-	public void setDateOfBirthDay(Date dateOfBirthDay) {
-		this.dateOfBirthDay = dateOfBirthDay;
-	}
-
-	public Date getTimeRegistration() {
-		return timeRegistration;
-	}
-
-	public void setTimeRegistration(Date timeRegistration) {
-		this.timeRegistration = timeRegistration;
-	}
-
-	public String getLastname() {
-		return lastname;
-	}
-
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-
-	public String getTelephone() {
-		return telephone;
-	}
-
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getCompany() {
-		return company;
-	}
-
-	public void setCompany(String company) {
-		this.company = company;
-	}
-
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", firstName=" + firstName + ", lastname=" + lastname + ", telephone=" + telephone
-				+ ", company=" + company + ", email=" + email + ", dateOfBirthDay=" + dateOfBirthDay
-				+ ", timeRegistration=" + timeRegistration + "]";
-	}
-
-	public String getCounty() {
-		return county;
-	}
-
-	public void setCounty(String county) {
-		this.county = county;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
 	public static long getSerialversionuid() {
 		return serialVersionUID;
-	}
-
-	public List<String> getScopeOfActivities() {
-		return scopeOfActivities;
-	}
-
-	public void setScopeOfActivities(List<String> scopeOfActivities) {
-		this.scopeOfActivities = scopeOfActivities;
-	}
-
-	public List<String> getSubscription() {
-		return subscription;
-	}
-
-	public void setSubscription(List<String> subscription) {
-		this.subscription = subscription;
 	}
 
 }
--- src/main/java/com/printmaster/nk/model/entity/UserAddByAdmin.java
+++ src/main/java/com/printmaster/nk/model/entity/UserAddByAdmin.java
@@ -1,57 +1,13 @@
 package com.printmaster.nk.model.entity;
 
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-
 import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
 
 @Entity
 @Table(name="user_add_by_admin")
-public class UserAddByAdmin implements Serializable{
-	private static final long serialVersionUID = 1L;
-
-	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
-	@Column(name="id")
-	private Long id;
-	
-	@Column(name="firstname")
-	private String firstName;
-	
-	@Column(name="lastname")
-	private String lastname;
-	
-	@Column(name="county")
-	private String county;
-	
-	@Column(name="city")
-	private String city;
-	
-	@Column(nullable = false, columnDefinition = "bit default 1")
-	private boolean enabled;
-	
-	@Column(name="telephone")
-	private String telephone;
-	
-	@Column(name="company")
-	private String company;
-	
-	@Column(name="email", unique = true)
-	private String email;
+public class UserAddByAdmin extends AbstractUser {
+	private static final long serialVersionUID = -2962121413734525491L;
 	
 	@Column(name="email2")
 	private String email2;
@@ -59,131 +15,10 @@ public class UserAddByAdmin implements Serializable{
 	@Column(name="email3")
 	private String email3;
 	
-	@Column(name="date_of_birth_bay")
-	private Date dateOfBirthDay;
-	
-	@Column(name="time_registration")
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date timeRegistration = new Date();
-	
-	@ElementCollection(fetch = FetchType.EAGER)
-	@Column(name="subscription")
-	@Fetch(value = FetchMode.SUBSELECT)
-	private List<String> subscription = new ArrayList<String>();
-	
-	@ElementCollection(fetch = FetchType.EAGER)
-	@Column(name="scopeOfActivities")
-	@Fetch(value = FetchMode.SUBSELECT)
-	private List<String> scopeOfActivities = new ArrayList<String>();
-	
 	public UserAddByAdmin(){}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastname() {
-		return lastname;
-	}
-
-	public void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-
-	public boolean isEnabled() {
-		return enabled;
-	}
-
-	public void setEnabled(boolean enabled) {
-		this.enabled = enabled;
-	}
-
-	public String getTelephone() {
-		return telephone;
-	}
-
-	public void setTelephone(String telephone) {
-		this.telephone = telephone;
-	}
-
-	public String getCompany() {
-		return company;
-	}
-
-	public void setCompany(String company) {
-		this.company = company;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Date getDateOfBirthDay() {
-		return dateOfBirthDay;
-	}
-
-	public void setDateOfBirthDay(Date dateOfBirthDay) {
-		this.dateOfBirthDay = dateOfBirthDay;
-	}
-
-	public Date getTimeRegistration() {
-		return timeRegistration;
-	}
-
-	public void setTimeRegistration(Date timeRegistration) {
-		this.timeRegistration = timeRegistration;
-	}
-
-	public List<String> getSubscription() {
-		return subscription;
-	}
-
-	public void setSubscription(List<String> subscription) {
-		this.subscription = subscription;
-	}
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
-	}
-
-	public String getCounty() {
-		return county;
-	}
-
-	public void setCounty(String county) {
-		this.county = county;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public List<String> getScopeOfActivities() {
-		return scopeOfActivities;
-	}
-
-	public void setScopeOfActivities(List<String> scopeOfActivities) {
-		this.scopeOfActivities = scopeOfActivities;
 	}
 
 	public String getEmail2() {
